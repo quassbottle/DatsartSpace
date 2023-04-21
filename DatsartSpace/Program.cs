b55bedf86ec6f3be8a89e0a03fb804c0a31ba5d2
quassbottle <no@bitches.com>
2023-04-21 17:14:11 +0300

    httpClient setup

--- DatsartSpace/Program.cs
+++ DatsartSpace/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Net.Http.Headers;
+
+var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("https://api.datsart.dats.art/")
+};
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "643cd453323f8643cd453323fa");
+
+//httpClient.PostAsync("/art/stage/next", )
