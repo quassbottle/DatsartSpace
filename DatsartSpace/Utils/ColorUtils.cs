6127ed1aac5d7cfacb4a3f7ab4ee236f3276dc29
quassbottle <no@bitches.com>
2023-04-22 01:58:38 +0300

    ColorUtils, frontend

--- DatsartSpace/Utils/ColorUtils.cs
+++ DatsartSpace/Utils/ColorUtils.cs
@@ -15,4 +15,23 @@ public static class ColorUtils
     {
         return (color1 + color2) / 2;
     }
+
+    public static (int, int, int) GetRgb(int mapInfo)
+    {
+        int red = mapInfo / 65536;
+        int green = (mapInfo - red * 65536) / 256;
+        int blue = mapInfo - red * 65536 - green * 256;
+
+        return (red, green, blue);
+    }
+
+    public static bool AreSimilar(int color1, int color2, int delta)
+    {
+        var rgb1 = GetRgb(color1);
+        var rgb2 = GetRgb(color2);
+
+        return Math.Abs(rgb1.Item1 - rgb2.Item1) <= delta &&
+               Math.Abs(rgb1.Item2 - rgb2.Item2) <= delta &&
+               Math.Abs(rgb1.Item3 - rgb2.Item3) <= delta;
+    }
 }
