e4361ebf636a6c7f1db502f780bc65342454f944
Kosty <kosty.lygin.58@mail.ru>
2023-04-22 13:26:37 +0300

    ballista

--- DatsartSpace/Ballista.cs
+++ DatsartSpace/Ballista.cs
@@ -3,45 +3,29 @@ namespace DatsartSpace;
 public class Ballista
 {
     public (double angleHorizontal, double angleVertical, double power) 
-        CalculateProjectile(double targetX, double targetY, double projectileMass, double paintArea)
+        CalculateProjectile(int targetX, int targetY, int valume)
     {
+        // Константы
         const double gravity = 9.80665;
         const double pi = 3.1415926535898;
         const double massCoefficient = 0.001;
 
-        // Calculate initial velocity based on the target coordinates and projectile mass
-        double initialVelocity = Math.Sqrt(2 * gravity * targetY) / Math.Sin(pi / 4);
+        // Исходные данные
+        double startX = Math.Abs(250 / 2 - targetX);;
+        double startY = targetY + 300;
+        double projectileMass = 1; // Произвольно выбранная масса снаряда
+        double initialSpeed = 0; // Начальная скорость, которую будем подбирать
 
-        // Calculate angle of elevation
-        double angleVertical = Math.Atan((2 * targetY) / (initialVelocity * initialVelocity));
+        double angleHorizontal = Math.Atan(startX / startY);
+        
+        double lenght = Math.Sqrt(Math.Pow(startX, 2) + Math.Pow(startY, 2));
 
-        // Calculate horizontal distance to target
-        double targetDistance = targetX - (-300);
+        double power = lenght * gravity * massCoefficient * valume / (2 * Math.Pow(
+            Math.Sin(0.785398), 2));
 
-        // Calculate time of flight
-        double timeOfFlight = targetDistance / (initialVelocity * Math.Cos(angleVertical));
-
-        // Calculate maximum height reached
-        double maxHeight = (initialVelocity * initialVelocity * Math.Sin(angleVertical) * Math.Sin(angleVertical)) / (2 * gravity);
-
-        // Calculate angle of impact
-        double angleImpact = Math.Atan((targetY - maxHeight) / targetDistance);
-
-        // Calculate horizontal velocity
-        double horizontalVelocity = initialVelocity * Math.Cos(angleVertical);
-
-        // Calculate horizontal distance covered during descent
-        //double descentDistance = (initialVelocity * Math.Sin(angleVertical) * timeOfFlight) - ((0.5) * gravity * timeOfFlight * timeOfFlight);
-
-        // Calculate impact velocity
-        double impactVelocity = Math.Sqrt((2 * gravity * (maxHeight - paintArea)) + (horizontalVelocity * horizontalVelocity));
-
-        // Calculate angle of impact velocity
-        double angleHorizontal = Math.Atan((impactVelocity * Math.Sin(angleImpact)) / (impactVelocity * Math.Cos(angleImpact)));
-
-        // Calculate power based on projectile mass and volume
-        double power = projectileMass * massCoefficient; // * projectileVolume;
-
-        return (angleHorizontal, angleVertical, power);
+        if (targetX < 250)
+            angleHorizontal = -angleHorizontal;
+        
+        return (angleHorizontal * 180 / pi, 45, power);
     }
 }
