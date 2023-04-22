fab1f583294470933931e45e88378a414d3b1f61
quassbottle <no@bitches.com>
2023-04-22 11:12:00 +0300

    dominant color fixed

--- ColorsGrinder/Program.cs
+++ ColorsGrinder/Program.cs
@@ -3,10 +3,24 @@
 var api = new DatsSpaceApi();
 var rnd = new Random();
 
+int colors = 0;
+int total = 0;
+Console.Title = $"Colors: {colors} | Total: {total}";
 while (true)
 {
-    var generateResult = await api.Factory.GenerateAsync();
-    var pick = await api.Factory.PickAsync(rnd.Next(1, 4), generateResult.Item2);
-    Console.WriteLine($"Picked: {pick.Color}; Amount: {pick.Amount}");
-    await Task.Delay(1000);
+    try
+    {
+        var generateResult = await api.Factory.GenerateAsync();
+        var pick = await api.Factory.PickAsync(rnd.Next(1, 4), generateResult.Item2);
+        Console.WriteLine($"Picked: {pick.Color}; Amount: {pick.Amount}");
+        colors++;
+        total += pick.Amount;
+        await Task.Delay(1000);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Died: " + ex.ToString());
+        await Task.Delay(10000);
+    }
+    Console.Title = $"Colors: {colors} | Total: {total}";
 }
